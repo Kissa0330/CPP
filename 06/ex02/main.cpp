74be739ec2d71113ba5accc483aa5eba76b36ea9
らいか <raika4789@gmail.com>
2023-03-08 17:02:12 +0900

    CPP06 retry

--- 06/ex02/main.cpp
+++ 06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rtakano <rtakano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 18:47:08 by rtakano           #+#    #+#             */
-/*   Updated: 2023/03/08 15:37:57 by rtakano          ###   ########.fr       */
+/*   Updated: 2023/03/08 17:01:50 by rtakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime> 
-#include <typeinfo>
 
 Base *generate(void)
 {
@@ -59,7 +58,7 @@ void identify(Base &p)
 		(void)dynamic_cast<A &>(p);
 		std::cout << "A" << std::endl;
 	}
-	catch (std::bad_cast)
+	catch (std::exception)
 	{
 	}
 
@@ -68,7 +67,7 @@ void identify(Base &p)
 		(void)dynamic_cast<B &>(p);
 		std::cout << "B" << std::endl;
 	}
-	catch (std::bad_cast)
+	catch (std::exception)
 	{
 	}
 
@@ -77,7 +76,7 @@ void identify(Base &p)
 		(void)dynamic_cast<C &>(p);
 		std::cout << "C" << std::endl;
 	}
-	catch (std::bad_cast)
+	catch (std::exception)
 	{
 	}
 }
