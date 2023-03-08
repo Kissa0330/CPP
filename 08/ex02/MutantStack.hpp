fed45a58f2b7d76b4ea999168b5c1eabb19c4f97
らいか <raika4789@gmail.com>
2023-03-08 22:55:29 +0900

    CPP08 ex02 finish

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: rtakano <rtakano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 13:28:39 by rtakano           #+#    #+#             */
-/*   Updated: 2023/03/08 14:38:51 by rtakano          ###   ########.fr       */
+/*   Updated: 2023/03/08 22:47:22 by rtakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,37 @@
 #include <stack>
 
 template <typename T>
-class MutantStack : public std::stack
+class MutantStack : public std::stack<T>
 {
 private:
-	/* data */
 public:
-	MutantStack(/* args */);
-	~MutantStack();
+	MutantStack() : std::stack<T>(){};
+	~MutantStack(){};
+	MutantStack(const MutantStack &origin) : std::stack<T>(origin){};
+	MutantStack<T> &operator=(const MutantStack<T> &origin)
+	{
+		if (*this != origin)
+			static_cast< std::stack<T> >(*this) = static_cast< std::stack<T> >(origin);
+		return (*this);
+	}
+
+	typedef typename std::stack<T>::container_type container_type;
+	typedef typename container_type::iterator iterator;
+	typedef typename container_type::const_iterator const_iterator;
+	typedef typename container_type::reverse_iterator reverse_iterator;
+	typedef typename container_type::const_reverse_iterator const_reverse_iterator;
+
+	iterator begin() { return (this->c.begin()); }
+	iterator end() { return (this->c.end()); }
+
+	const_iterator begin() const { return (this->c.begin()); }
+	const_iterator end() const { return (this->c.end()); }
+
+	reverse_iterator rbegin() { return (this->c.rbegin()); }
+	reverse_iterator rend() { return (this->c.rend()); }
+
+	const_reverse_iterator rbegin() const { return (this->c.rbegin()); }
+	const_reverse_iterator rend() const { return (this->c.rend()); }
 };
 
 #endif
--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,13 +6,68 @@
 /*   By: rtakano <rtakano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 13:28:19 by rtakano           #+#    #+#             */
-/*   Updated: 2023/03/08 13:28:32 by rtakano          ###   ########.fr       */
+/*   Updated: 2023/03/08 22:54:45 by rtakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <iostream>
 
 int main()
 {
+	// basic_test
+	{
+		std::cout << "basic_test" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		std::cout << "mstack" << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		MutantStack<int> mstack2(mstack);
+		MutantStack<int>::iterator it2 = mstack2.begin();
+		MutantStack<int>::const_iterator c_ite = mstack2.end();
+		std::cout << "mstack2" << std::endl;
+		while (it2 != c_ite)
+		{
+			std::cout << *it2 << std::endl;
+			++it2;
+		}
+		MutantStack<int> mstack3 = mstack;
+		MutantStack<int>::iterator it3 = mstack2.begin();
+		MutantStack<int>::const_iterator ite3 = mstack2.end();
+		std::cout << "mstack3" << std::endl;
+		while (it3 != ite3)
+		{
+			std::cout << *it3 << std::endl;
+			++it3;
+		}
+		
+		std::stack<int> s(mstack);
+	}
+	// self_assignment_test
+	{
+		MutantStack<int> *mstack1 = new MutantStack<int>();
+		MutantStack<int> *mstack2 = NULL;
+
+		(*mstack1).push(1);
+		mstack1 = mstack2;
+		mstack2 = mstack1;
+		delete mstack1;
+	}
 	return 0;
 }
